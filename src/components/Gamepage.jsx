6d05d86c909ad97176c46af7e715310e367d85a9
Andrew Newland <anewland@gmail.com>
2020-12-31 13:40:36 -0600

    pulling in via round param

--- src/components/Gamepage.jsx
+++ src/components/Gamepage.jsx
@@ -2,26 +2,42 @@ import React, { Component } from 'react';
 // import { Link } from 'react-router-dom';
 import axios from 'axios';
 
+let getParams = function (url) {
+	var params = {};
+	var parser = document.createElement('a');
+	parser.href = url;
+	var query = parser.search.substring(1);
+	var vars = query.split('&');
+	for (var i = 0; i < vars.length; i++) {
+		var pair = vars[i].split('=');
+		params[pair[0]] = decodeURIComponent(pair[1]);
+	}
+	return params;
+};
+
 export class Page extends Component {
   state = {
-    podcasts: [],
+    truths: [],
+    rounds: 0,
     isLoaded: false,
   }
 
   componentDidMount() {
-
-    var search = window.location.search.substring(1);
-    JSON.parse('{"' + search.replace(/&/g, '","').replace(/=/g,'":"') + '"}', function(key, value) { return key===""?value:decodeURIComponent(value) });
-
-    console.log(search);
+    let params = getParams(window.location.href);
 
     const api = 'https://admin.ithinkpixels.com/api/collections/get';
     const token = '148bed39568ed0436384f2e90bbe53';
 
-    axios.get(api+'/toy_truths?token='+token).then(res => {
-      console.dir(res);
+    axios.get(api+'/toy_truths?token='+token+'&filter[episode]='+params['episode']+'&filter[round]='+params['round']).then(res => {
+      // console.dir(res);
+      const epTruths = [];
+      res.data.entries.forEach(element => {
+        epTruths.push(element);
+      });
 
       this.setState({
+        truths: epTruths,
+        rounds: Math.ceil(epTruths.length / 3),
         isLoaded: true
       })
     })
@@ -29,6 +45,9 @@ export class Page extends Component {
   }
 
   render() {
+    let { truths } = this.state;
+    let params = getParams(window.location.href);
+
     return (
       <>
         <section className="content">
@@ -36,7 +55,21 @@ export class Page extends Component {
             <div className="row">
               <div className="col-12">
                 <h2>TOY TRUTHS</h2>
+                <h3>EPISODE { params['episode'] } | ROUND { params['round'] }</h3>
               </div>
+
+              <div className="col-12">
+                <button>Round 1</button>
+                <button>Round 2</button>
+              </div>
+            </div>
+
+            <div className="row">
+              {truths.map(t =>
+                <div className="col-4">
+                  <p key={ t._id } dangerouslySetInnerHTML={{ __html: t.fact }} />
+                </div>
+              )}
             </div>
           </div>
         </section>
