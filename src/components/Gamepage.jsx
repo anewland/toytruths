ab00c4cfaa8452babb2221d5bfddc140032a1afe
Andrew Newland <anewland@gmail.com>
2021-01-03 21:48:41 -0600

    Game 1 complete

--- src/components/Gamepage.jsx
+++ src/components/Gamepage.jsx
@@ -29,19 +29,27 @@ export class Page extends Component {
     const token = '148bed39568ed0436384f2e90bbe53';
 
     axios.get(api+'/toy_truths?token='+token+'&filter[episode]='+params['episode']+'&filter[round]='+params['round']).then(res => {
-      // console.dir(res);
+      console.dir(res);
       const epTruths = [];
       res.data.entries.forEach(element => {
         epTruths.push(element);
       });
 
       this.setState({
-        truths: epTruths,
+        truths: epTruths.sort(() => Math.random() - 0.5),
         rounds: Math.ceil(epTruths.length / 3),
         isLoaded: true
       })
     })
     .catch(err => console.log(err));
+  }
+
+  clicked(e) {
+    e.target.classList.add('clicked');
+  }
+
+  answer(e) {
+    e.target.parentElement.classList.add('cont');
   }
 
   render() {
@@ -58,16 +66,23 @@ export class Page extends Component {
                 <h3>EPISODE { params['episode'] } | ROUND { params['round'] }</h3>
               </div>
 
-              <div className="col-12">
-                <button>Round 1</button>
-                <button>Round 2</button>
+              <div className="col-12 buttons">
+                <a href="/?episode=23&round=1">Round 1</a>
+                <a href="/?episode=23&round=2">Round 2</a>
+                <a href="/?episode=23&round=3">Round 3</a>
+                <a href="/?episode=23&round=4">Round 4</a>
+                {/* <a href="/?episode=23&round=5">Round 5</a> */}
+                {/* <a href="/?episode=23&round=6">Round 6</a> */}
               </div>
             </div>
 
             <div className="row">
               {truths.map(t =>
-                <div className="col-4">
-                  <p key={ t._id } dangerouslySetInnerHTML={{ __html: t.fact }} />
+                <div className="col-12 fact">
+                  <div className={ t.status } onClick={ this.clicked }>
+                    <p key={ t._id } dangerouslySetInnerHTML={{ __html: t.fact }} />
+                    <button className="answer" onClick={ this.answer }><span>A</span></button>
+                  </div>
                 </div>
               )}
             </div>
